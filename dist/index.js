c7637d6b690b0aa416026ae9f3a10e59906dc267
Sam Parry <67956822+xmenbteam@users.noreply.github.com>
2022-06-22 14:49:05 +0100

    Changed API

--- dist/index.js
+++ dist/index.js
@@ -7,21 +7,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.thingToPrint = void 0;
 const axios_1 = __importDefault(require("axios"));
 exports.thingToPrint = process.argv[2];
-console.log(process.argv);
-const printThing = async (thing) => {
-    console.log(`Hello ${thing}! Fetching your dad joke...`);
+const printThing = async (name) => {
+    console.log(`Hello ${name}! Fetching your dad joke... \n`);
     try {
         const response = await axios_1.default.request({
             method: "GET",
             url: "https://dad-jokes.p.rapidapi.com/random/joke",
             headers: {
-                "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
                 "X-RapidAPI-Key": "220c0cd0cbmsh0add015af228208p1b55d8jsnb4a19430d21b",
+                "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
             },
         });
         // const body = await response.json();
-        console.log(response.data.body[0].setup);
-        console.log(response.data.body[0].punchline);
+        console.log(response.data.body[0].setup, "\n");
+        console.log(response.data.body[0].punchline, "\n");
     }
     catch (err) {
         console.log(">>>>", err, "<<<");
--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,20 @@ import axios, { AxiosResponse } from "axios";
 export const thingToPrint = process.argv[2];
 
 const printThing = async (name: string) => {
-  console.log(`Hello ${name}! Fetching your dad joke...`);
+  console.log(`Hello ${name}! Fetching your dad joke... \n`);
   try {
     const response: AxiosResponse<any> = await axios.request({
       method: "GET",
       url: "https://dad-jokes.p.rapidapi.com/random/joke",
       headers: {
-        "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
         "X-RapidAPI-Key": "220c0cd0cbmsh0add015af228208p1b55d8jsnb4a19430d21b",
+        "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
       },
     });
     // const body = await response.json();
 
-    console.log(response.data.body[0].setup);
-    console.log(response.data.body[0].punchline);
+    console.log(response.data.body[0].setup, "\n");
+    console.log(response.data.body[0].punchline, "\n");
   } catch (err) {
     console.log(">>>>", err, "<<<");
   }
